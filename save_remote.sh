a41909605525bea884fa72600b777e277f1c9eb3
ArdaTrkmnoglu <ardatrkmnoglu@outlook.com>
2026-02-23 23:15:10 +0300

    fixed: execution path for remote script over SSH

--- save_remote.sh
+++ save_remote.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 URL="$1"
+# This is the path of dependency 'yt-dlp'.
+# Please check the output of the command `which yt-dlp`.
+# If the path is different, change this value with the parent folder of 'yt-dlp'.
+POS="/Library/Frameworks/Python.framework/Versions/3.13/bin/"
+export PATH="$POS:/usr/local/bin:/usr/bin:/usr/sbin:/sbin"
 
 yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" \
        --merge-output-format mp4 \
